8f9438fdb918b78421703fd456012bb4bd3ed214
kavindraD <kavindra.dubey@rediffmail.com>
2014-12-16 21:01:43 +0530

    Updated
    Added copyright header..

--- simple/Html5VideoPlayer/eventHandlers/build/devtripVideo.js
+++ simple/Html5VideoPlayer/eventHandlers/build/devtripVideo.js
@@ -1,3 +1,18 @@
+/**
+*
+Copyright (c) 2014-15 Kavindra Dubey - kavindra.dubey@gmail.com [personal site - devtrip.com]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Except as contained in this notice, the name(s) of the above copyright holders shall not be used in advertising or otherwise to promote the sale, use or other dealings in this Software without prior written authorization.
+
+The end-user documentation included with the redistribution, if any, must include the following acknowledgment: "This product includes software developed by The DevtripVideo Project, (http://www.devtrip.com/) and its contributors", in the same place and form as other third-party acknowledgments. Alternately, this acknowledgment may appear in the software itself, in the same form and location as other such third-party acknowledgments.
+*
+**/
 var devtripVideo=function(){function e(a,c){var b=a.canPlayType(c);if("maybe"==b)return c;if(""==b)return""}function c(a,c,b,d){window.addEventListener?a.addEventListener(c,b,d):document.attachEvent?a.attachEvent("on"+c,b):a["on"+c]=b}function d(a,c,b,d){window.removeEventListener?a.removeEventListener(c,b,d):document.detachEvent?a.detachEvent("on"+c,b):a["on"+c]=null}function k(a){switch(a.target.error.code){case a.target.error.MEDIA_ERR_ABORTED:alert("You aborted the video playback.");break;case a.target.error.MEDIA_ERR_NETWORK:alert("A network error caused the video download to fail part-way.");
 break;case a.target.error.MEDIA_ERR_DECODE:alert("The video playback was aborted due to a corruption problem or because the video used features your browser did not support.");break;case a.target.error.MEDIA_ERR_SRC_NOT_SUPPORTED:alert("The video could not be loaded, either because the server or network failed or because the format is not supported.");break;default:alert("An unknown error occurred.")}l.call()}function m(a){n.call()}function p(a){q.call()}function r(a){s.call()}function t(a){u.call()}
 function v(a){w.call()}function x(a){y.call()}function z(a){A.call()}function B(a){C.call()}function D(a){E.call()}function F(a){G.call()}function H(a){I.call()}function J(a){K.call()}function L(a){M.call()}function N(a){O.call()}var h="assets/",l=function(){},n=function(){},q=function(){},s=function(){},u=function(){},w=function(){},y=function(){},A=function(){},C=function(){},E=function(){},G=function(){},I=function(){},K=function(){},M=function(){},O=function(){};return{init:function(a){var d=
